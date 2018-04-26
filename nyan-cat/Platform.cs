c288baaeac7fe75fddd777b9015705e740eb76e8
odinochka3000 <38666896+odinochka3000@users.noreply.github.com>
2018-04-26 23:28:52 +0500

    Update Platform.cs

--- nyan-cat/Platform.cs
+++ nyan-cat/Platform.cs
@@ -5,14 +5,31 @@ namespace nyan_cat
 {
     public class Platform : IGameObject
     {
+        public int Height = 10;
         public Vector2 Velocity { get; }
-        public Point Center { get; }
+        public Point Center { get; private set; }
         public int Length { get; }
+        public Point RightTopCorner { get; private set; }
 
         public Platform(Point center, int length)
         {
             Center = center;
             Length = length;
+            RightTopCorner = GetRightTopCorner(Center);
+            Velocity = new Vector2(-1, 0);
+        }
+
+        public void Move()
+        {
+            Center = new Point(Center.X - (int)Velocity.X,
+                Center.Y - (int)Velocity.Y);
+            RightTopCorner = GetRightTopCorner(Center);
+        }
+
+        private Point GetRightTopCorner(Point center)
+        {
+            return new Point(center.X - Length / 2,
+                center.Y - Height / 2);
         }
     }
 }
