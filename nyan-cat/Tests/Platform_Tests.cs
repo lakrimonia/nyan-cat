e5498f291341d9004743380aef9857ae83778770
odinochka3000 <38666896+odinochka3000@users.noreply.github.com>
2018-04-28 02:10:55 +0500

    Update Platform_Tests.cs
    Тесты, относящиеся к инициализации. В данный момент из 4 тестов проходит только один, требуется добавить проверки инициализации в класс Platform(планирую заняться этим). Также нужно объединить эти тесты в группу тестов.

--- nyan-cat/Tests/Platform_Tests.cs
+++ nyan-cat/Tests/Platform_Tests.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using System.Drawing;
 
 namespace nyan_cat.Tests
 {
@@ -11,9 +12,29 @@ namespace nyan_cat.Tests
     public class Platform_Tests
     {
         [Test]
-        public void SomeTest()
+        public void ZeroWidth()
         {
+            Assert.Throws<ArgumentNullException>(() => { CreatePlatform(1, 1, 0); });
+        }
+        [Test]
+        public void EvenWidth()
+        {
+            Assert.Throws<ArgumentNullException>(() => { CreatePlatform(1, 1, 10); });
+        }
+        [Test]
+        public void CenterOutField()
+        {
+            Assert.Throws<ArgumentNullException>(() => { CreatePlatform(-1, 1, 11); });
+        }
+        [Test]
+        public void CorrectCreation()
+        {
+            var platform = CreatePlatform(50, 50, 11);
+        }
 
+        public Platform CreatePlatform(int x, int y, int width)
+        {
+            return new Platform(new Point(x, y), width);
         }
     }
 }
