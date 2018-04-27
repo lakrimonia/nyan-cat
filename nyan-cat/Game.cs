98d0e436972a77b79a6f2054fd9337d6abe1a637
odinochka3000 <38666896+odinochka3000@users.noreply.github.com>
2018-04-27 22:17:24 +0500

    Update Game.cs

--- nyan-cat/Game.cs
+++ nyan-cat/Game.cs
@@ -13,10 +13,13 @@ namespace nyan_cat
         public int Score { get; }
         public int Combo { get; }
         public bool IsOver { get; }
+        public IGameObject[,] Map { get; }
+        public List<IGameObject> GameObjects { get; }
 
         public Game()
         {
             NyanCat = new NyanCat(new Point(0, 0));
+            Map = MapCreator.CreateRandomMap();
         }
     }
 }
