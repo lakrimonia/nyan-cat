5a08220564c50674943d567c030b4458008f88b0
lakrimonia <anasaturday@yandex.ru>
2018-04-27 18:59:35 +0500

    Tests
    * добавлены файлы с тестами.

---
+++ nyan-cat/Tests/MapCreator_Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace nyan_cat.Tests
+{
+    [TestFixture]
+    public class MapCreator_Tests
+    {
+        [Test]
+        public void SomeTest()
+        {
+
+        }
+    }
+}
---
+++ nyan-cat/Tests/NyanCat_Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace nyan_cat.Tests
+{
+    [TestFixture]
+    public class NyanCat_Tests
+    {
+        [Test]
+        public void SomeTest()
+        {
+
+        }
+    }
+}
