0a0ffeee1024943959bbefdf25a00738bf01849e
odinochka3000 <38666896+odinochka3000@users.noreply.github.com>
2018-04-27 23:21:38 +0500

    Update Platform.cs
    Added condition of extinction and the algorithm of motion at the left edge.

--- nyan-cat/Platform.cs
+++ nyan-cat/Platform.cs
@@ -1,15 +1,16 @@
-﻿using System.Drawing;
+using System;
+using System.Drawing;
 using System.Numerics;
 
 namespace nyan_cat
 {
     public class Platform : IGameObject
     {
-        public bool IsAlive { get; }
+        public bool IsAlive { get; private set; }
         public int Height { get; }
         public Vector2 Velocity { get; }
         public Point Center { get; private set; }
-        public int Width { get; }
+        public int Width { get; private set; }
         public Point RightTopCorner { get; private set; }
 
         public Platform(Point center, int width)
@@ -24,8 +25,18 @@ namespace nyan_cat
 
         public void Move()
         {
-            Center = new Point(Center.X - (int)Velocity.X,
-                Center.Y - (int)Velocity.Y);
+            var dx = (int)Velocity.X;
+            var dy = (int)Velocity.Y;
+            IsAlive = RightTopCorner.X + Width <= Math.Abs(dx);
+            if (!IsAlive)
+                return;
+            if (RightTopCorner.X + dx < 0)
+            {
+                Width -= dx;
+                dx = dx / 2 + 2;
+            }
+            Center = new Point(Center.X + dx,
+                Center.Y + dy);
             RightTopCorner = GetRightTopCorner(Center);
         }
 
