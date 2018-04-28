6ab2a996a3eeb3cc90cd9999acbdd9c0497f9413
odinochka3000 <38666896+odinochka3000@users.noreply.github.com>
2018-04-29 03:34:37 +0500

    Update Gem_Tests.cs
    - добавлены стандартные тесты

--- nyan-cat/Tests/Gem_Tests.cs
+++ nyan-cat/Tests/Gem_Tests.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +12,42 @@ namespace nyan_cat.Tests
     public class Gem_Tests
     {
         [Test]
-        public void SomeTest()
+        public void IncorrectCreation()
         {
+            Assert.Throws<ArgumentException>(() => { CreateDoubleGem(-1, 0); });
+        }
 
+        [Test]
+        public void CorrectCreation()
+        {
+            var gem = CreateDoubleGem(3, 3);
+        }
+
+        [Test]
+        public void Move()
+        {
+            var gem = CreateDoubleGem(10, 10);
+            gem.Move();
+            Assert.AreEqual(new Point(9, 10), gem.LeftTopCorner);
+        }
+
+        [Test]
+        public void OverEdge()
+        {
+            var gem = CreateDoubleGem(3, 3);
+            Move(gem, 4);
+            Assert.AreEqual(false, gem.IsAlive, gem.ToString());
+        }
+
+        public Gem CreateDoubleGem(int x , int y)
+        {
+            return new Gem(new Point(x, y), GemKind.DoubleCombo);
+        }
+
+        public void Move(Gem food, int count)
+        {
+            for (var i = 0; i < count; i++)
+                food.Move();
         }
     }
 }
