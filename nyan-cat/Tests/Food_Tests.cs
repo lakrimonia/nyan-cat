59a62fb5bbfcaf8ce30f6ef5e6c8279d0ccb0e94
odinochka3000 <38666896+odinochka3000@users.noreply.github.com>
2018-04-29 03:27:36 +0500

    Update Food_Tests.cs
    - добавлены стандартные тесты

--- nyan-cat/Tests/Food_Tests.cs
+++ nyan-cat/Tests/Food_Tests.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +12,37 @@ namespace nyan_cat.Tests
     public class Food_Tests
     {
         [Test]
-        public void SomeTest()
+        public void IncorrectCreation()
         {
+            Assert.Throws<ArgumentException>(() => { new Food(new Point(-1, 0)); });
+        }
 
+        [Test]
+        public void CorrectCreation()
+        {
+            var food = new Food(new Point(3, 3));
+        }
+
+        [Test]
+        public void Move()
+        {
+            var food = new Food(new Point(10, 10));
+            food.Move();
+            Assert.AreEqual(new Point(9, 10), food.LeftTopCorner);
+        }
+
+        [Test]
+        public void OverEdge()
+        {
+            var food = new Food(new Point(3, 3));
+            Move(food, 4);
+            Assert.AreEqual(false, food.IsAlive, food.ToString());
+        }
+
+        public void Move(Food food, int count)
+        {
+            for (var i = 0; i < count; i++)
+                food.Move();
         }
     }
 }
