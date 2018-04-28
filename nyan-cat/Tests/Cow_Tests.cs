d9e4cd1d7307818838b0cf6d31a0cef9da92469f
odinochka3000 <38666896+odinochka3000@users.noreply.github.com>
2018-04-29 03:18:43 +0500

    Update Cow_Tests.cs
     - добавлены стандартные тесты

--- nyan-cat/Tests/Cow_Tests.cs
+++ nyan-cat/Tests/Cow_Tests.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +12,37 @@ namespace nyan_cat.Tests
     public class Cow_Tests
     {
         [Test]
-        public void SomeTest()
+        public void IncorrectCreation()
         {
+            Assert.Throws<ArgumentException>(() => { new Cow(new Point(-1, 0)); });
+        }
 
+        [Test]
+        public void CorrectCreation()
+        {
+            var cow = new Cow(new Point(3, 3));
+        }
+
+        [Test]
+        public void Move()
+        {
+            var cow = new Cow(new Point(10, 10));
+            cow.Move();
+            Assert.AreEqual(new Point(9, 10), cow.LeftTopCorner);
+        }
+
+        [Test]
+        public void OverEdge()
+        {
+            var cow = new Cow(new Point(3, 3));
+            Move(cow, 4);
+            Assert.AreEqual(false, cow.IsAlive, cow.ToString());
+        }
+
+        public void Move(Cow cow, int count)
+        {
+            for (var i = 0; i < count; i++)
+                cow.Move();
         }
     }
 }
