f0f35a7b3216f4626690f860ba7701eb7675ad41
odinochka3000 <38666896+odinochka3000@users.noreply.github.com>
2018-04-29 02:47:49 +0500

    Update NyanCat_Tests.cs
    - добавлены тесты(все проходят проверку)

--- nyan-cat/Tests/NyanCat_Tests.cs
+++ nyan-cat/Tests/NyanCat_Tests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -12,9 +12,53 @@ namespace nyan_cat.Tests
     public class NyanCat_Tests
     {
         [Test]
-        public void SomeTest()
+        public void IncorrectCreation()
         {
+            Assert.Throws<ArgumentException>(() => { new NyanCat(new Point(-1, 0)); });
+        }
 
+        [Test]
+        public void CorrectCreation()
+        {
+            var cat = new NyanCat(new Point(3, 3));
+        }
+
+        [Test]
+        public void Run()
+        {
+            var cat = new NyanCat(new Point(10, 50));
+            cat.Move();
+            Assert.AreEqual(new Point(10, 50), cat.LeftTopCorner, cat.ToString());
+            Assert.AreEqual(CatState.Run, cat.State, cat.ToString());
+        }
+
+        [Test]
+        public void Jump()
+        {
+            var cat = new NyanCat(new Point(10, 50));
+            cat.Jump();
+            cat.Move();
+            Assert.AreEqual(new Point(10, 40), cat.LeftTopCorner, cat.ToString());
+            Assert.AreEqual(CatState.Jump, cat.State, cat.ToString());
+        }
+
+        [Test]
+        public void FallIfTouchTheTop()
+        {
+            var cat = new NyanCat(new Point(10, 5));
+            cat.Jump();
+            cat.Move();
+            Assert.AreEqual(new Point(10, 0), cat.LeftTopCorner, cat.ToString());
+            Assert.AreEqual(CatState.Fall, cat.State, cat.ToString());
+        }
+
+        [Test]
+        public void Death()
+        {
+            var cat = new NyanCat(new Point(10, 780));
+            cat.State = CatState.Fall;
+            cat.Move();
+            Assert.AreEqual(false, cat.IsAlive, cat.ToString());
         }
     }
 }
