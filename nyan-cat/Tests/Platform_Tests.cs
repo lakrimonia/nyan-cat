ca310b7c7323f56dde7c57fe220794e884b2f543
odinochka3000 <38666896+odinochka3000@users.noreply.github.com>
2018-04-28 17:44:46 +0500

    Update Platform_Tests.cs
    Новые тесты

--- nyan-cat/Tests/Platform_Tests.cs
+++ nyan-cat/Tests/Platform_Tests.cs
@@ -11,20 +11,35 @@ namespace nyan_cat.Tests
     [TestFixture]
     public class Platform_Tests
     {
+
         [TestCase(1, 1, 0)]
-        [TestCase(1, 1, 10)]
-        [TestCase(-1, 1, 11)]
+        [TestCase(-1, 1, 10)]
         public void IncorrectCreation(int xCenter, int yCenter, int width)
         {
-            Assert.Throws<ArgumentNullException>(() => { CreatePlatform(xCenter, yCenter, width); });
+            Assert.Throws<ArgumentException>(() => { CreatePlatform(xCenter, yCenter, width); });
         }
-
         [Test]
         public void CorrectCreation()
         {
-            var platform = CreatePlatform(50, 50, 11);
+            var platform = CreatePlatform(10, 10, 10);
         }
 
+        [Test]
+        public void Move()
+        {
+            var platform = CreatePlatform(10, 10, 10);
+            platform.Move();
+            Assert.AreEqual(new Point(9, 10), platform.LeftTopCorner);
+        }
+
+        [Test]
+
+        public void OverEdge()
+        {
+            var platform = CreatePlatform(0, 0, 1);
+            platform.Move();
+            Assert.AreEqual(false, platform.IsAlive);
+        }
         public Platform CreatePlatform(int x, int y, int width)
         {
             return new Platform(new Point(x, y), width);
