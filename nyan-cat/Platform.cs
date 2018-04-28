b95b871f11869904fba02b3d1b5c92ba590f8cb6
odinochka3000 <38666896+odinochka3000@users.noreply.github.com>
2018-04-28 17:43:56 +0500

    LeftTopCorner

--- nyan-cat/Platform.cs
+++ nyan-cat/Platform.cs
@@ -9,17 +9,18 @@ namespace nyan_cat
         public bool IsAlive { get; private set; }
         public int Height { get; }
         public Vector2 Velocity { get; }
-        public Point Center { get; private set; }
         public int Width { get; private set; }
-        public Point RightTopCorner { get; private set; }
+        public Point LeftTopCorner { get; private set; }
 
-        public Platform(Point center, int width)
+        public Platform(Point leftTopCorner, int width)
         {
+            if (width <= 0 || width >= 1000 ||
+                leftTopCorner.X < 0 || leftTopCorner.Y < 0)
+                throw new ArgumentException();
             IsAlive = true;
             Height = 26;
-            Center = center;
             Width = width;
-            RightTopCorner = GetRightTopCorner(Center);
+            LeftTopCorner = leftTopCorner;
             Velocity = new Vector2(-1, 0);
         }
 
@@ -27,23 +28,16 @@ namespace nyan_cat
         {
             var dx = (int)Velocity.X;
             var dy = (int)Velocity.Y;
-            IsAlive = RightTopCorner.X + Width <= Math.Abs(dx);
+            IsAlive = LeftTopCorner.X + Width > Math.Abs(dx);
             if (!IsAlive)
                 return;
-            if (RightTopCorner.X + dx < 0)
+            if (LeftTopCorner.X + dx < 0)
             {
                 Width -= dx;
                 dx = dx / 2 + 2;
             }
-            Center = new Point(Center.X + dx,
-                Center.Y + dy);
-            RightTopCorner = GetRightTopCorner(Center);
-        }
-
-        private Point GetRightTopCorner(Point center)
-        {
-            return new Point(center.X - Width / 2,
-                center.Y - Height / 2);
+            LeftTopCorner = new Point(LeftTopCorner.X + dx,
+                LeftTopCorner.Y + dy);
         }
     }
 }
