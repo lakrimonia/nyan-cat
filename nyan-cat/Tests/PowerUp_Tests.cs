fc293b162d1ec65ecff09b306f619fecb4d79c5c
odinochka3000 <38666896+odinochka3000@users.noreply.github.com>
2018-04-29 03:58:19 +0500

    Update PowerUp_Tests.cs
    - добавлены стандартные тесты

--- nyan-cat/Tests/PowerUp_Tests.cs
+++ nyan-cat/Tests/PowerUp_Tests.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +12,42 @@ namespace nyan_cat.Tests
     public class PowerUp_Tests
     {
         [Test]
-        public void SomeTest()
+        public void IncorrectCreation()
         {
+            Assert.Throws<ArgumentException>(() => { CreatePowerUp(-1, 0); });
+        }
 
+        [Test]
+        public void CorrectCreation()
+        {
+            var powerUp= CreatePowerUp(3, 3);
+        }
+
+        [Test]
+        public void Move()
+        {
+            var powerUp = CreatePowerUp(10, 10);
+            powerUp.Move();
+            Assert.AreEqual(new Point(9, 10), powerUp.LeftTopCorner);
+        }
+
+        [Test]
+        public void OverEdge()
+        {
+            var powerUp = CreatePowerUp(3, 3);
+            Move(powerUp, 4);
+            Assert.AreEqual(false, powerUp.IsAlive, powerUp.ToString());
+        }
+
+        public PowerUp CreatePowerUp(int x, int y)
+        {
+            return new PowerUp(new Point(x, y), PowerUpKind.TurboNyan);
+        }
+
+        public void Move(PowerUp powerUp, int count)
+        {
+            for (var i = 0; i < count; i++)
+                powerUp.Move();
         }
     }
 }
