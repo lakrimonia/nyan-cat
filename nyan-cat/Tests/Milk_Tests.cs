22136441cb60da79d78f9ecd76e07944728f18f8
odinochka3000 <38666896+odinochka3000@users.noreply.github.com>
2018-04-29 03:23:26 +0500

    Update Milk_Tests.cs

--- nyan-cat/Tests/Milk_Tests.cs
+++ nyan-cat/Tests/Milk_Tests.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +12,37 @@ namespace nyan_cat.Tests
     public class Milk_Tests
     {
         [Test]
-        public void SomeTest()
+        public void IncorrectCreation()
         {
+            Assert.Throws<ArgumentException>(() => { new Milk(new Point(-1, 0)); });
+        }
 
+        [Test]
+        public void CorrectCreation()
+        {
+            var milk = new Milk(new Point(3, 3));
+        }
+
+        [Test]
+        public void Move()
+        {
+            var milk = new Milk(new Point(10, 10));
+            milk.Move();
+            Assert.AreEqual(new Point(9, 10), milk.LeftTopCorner);
+        }
+
+        [Test]
+        public void OverEdge()
+        {
+            var milk = new Milk(new Point(3, 3));
+            Move(milk, 4);
+            Assert.AreEqual(false, milk.IsAlive, milk.ToString());
+        }
+
+        public void Move(Milk milk, int count)
+        {
+            for (var i = 0; i < count; i++)
+                milk.Move();
         }
     }
 }
