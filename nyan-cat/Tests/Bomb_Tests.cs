d62fd91116899f9dd1dd1bdedca9b749634c10b9
odinochka3000 <38666896+odinochka3000@users.noreply.github.com>
2018-04-29 03:00:03 +0500

    Update Bomb_Tests.cs
    - добавлены тесты

--- nyan-cat/Tests/Bomb_Tests.cs
+++ nyan-cat/Tests/Bomb_Tests.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +12,37 @@ namespace nyan_cat.Tests
     public class Bomb_Tests
     {
         [Test]
-        public void SomeTest()
+        public void IncorrectCreation()
         {
+            Assert.Throws<ArgumentException>(() => { new Bomb(new Point(-1, 0)); });
+        }
 
+        [Test]
+        public void CorrectCreation()
+        {
+            var bomb = new Bomb(new Point(3, 3));
+        }
+
+        [Test]
+        public void Move()
+        {
+            var bomb = new Bomb(new Point(10, 10));
+            bomb.Move();
+            Assert.AreEqual(new Point(9, 10), bomb.LeftTopCorner);
+        }
+
+        [Test]
+        public void OverEdge()
+        {
+            var bomb = new Bomb(new Point(3, 3));
+            Move(bomb, 4);
+            Assert.AreEqual(false, bomb.IsAlive, bomb.ToString());
+        }
+
+        public void Move(Bomb bomb, int count)
+        {
+            for (var i = 0; i < count; i++)
+                bomb.Move();
         }
     }
 }
