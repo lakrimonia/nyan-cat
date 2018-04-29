2f98de201451850b63e98de962657f8eff9b397e
odinochka3000 <38666896+odinochka3000@users.noreply.github.com>
2018-04-29 20:43:46 +0500

    Update UFO_Tests.cs
    - добавлены тесты

--- nyan-cat/Tests/UFO_Tests.cs
+++ nyan-cat/Tests/UFO_Tests.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +12,37 @@ namespace nyan_cat.Tests
     public class UFO_Tests
     {
         [Test]
-        public void SomeTest()
+        public void IncorrectCreation()
         {
+            Assert.Throws<ArgumentException>(() => { new UFO(new Point(-1, 0)); });
+        }
 
+        [Test]
+        public void CorrectCreation()
+        {
+            var ufo = new UFO(new Point(3, 3));
+        }
+
+        [Test]
+        public void Move()
+        {
+            var ufo = new UFO(new Point(10, 10));
+            ufo.Move();
+            Assert.AreEqual(new Point(9, 10), ufo.LeftTopCorner);
+        }
+
+        [Test]
+        public void OverEdge()
+        {
+            var ufo = new UFO(new Point(3, 3));
+            Move(ufo, 4);
+            Assert.AreEqual(false, ufo.IsAlive, ufo.ToString());
+        }
+
+        public void Move(UFO ufo, int count)
+        {
+            for (var i = 0; i < count; i++)
+                ufo.Move();
         }
     }
 }
