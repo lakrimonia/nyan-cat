4a5b8df277720b5d9602b1608ebdb5cc556cb448
odinochka3000 <38666896+odinochka3000@users.noreply.github.com>
2018-04-29 20:38:03 +0500

    Update Animal_Tests.cs
    - добавлены тесты

--- nyan-cat/Tests/Animal_Tests.cs
+++ nyan-cat/Tests/Animal_Tests.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +12,77 @@ namespace nyan_cat.Tests
     public class Animal_Tests
     {
         [Test]
-        public void SomeTest()
+        public void IncorrectCreation1()
         {
+            var platform = CreatePlatform(100, 100, 1);
+            Assert.Throws<ArgumentException>(() => { new Animal(platform); });
+        }
 
+        [Test]
+        public void IncorrectCreation2()
+        {
+            var platform = CreatePlatform(10, 10, 60);
+            Assert.Throws<ArgumentException>(() => { new Animal(platform); });
+        }
+
+        [Test]
+        public void CorrectCreation()
+        {
+            var platform = CreatePlatform(100, 100, 60);
+            var animal = new Animal(platform);
+        }
+
+        [Test]
+        public void LeftTopCorner()
+        {
+            var platform = CreatePlatform(100, 100, 60);
+            var animal = new Animal(platform);
+            Assert.AreEqual(new Point(100, 50),
+                animal.LeftTopCorner, animal.ToString());
+        }
+
+        [Test]
+        public void MoveIfOnTheLeftEdge()
+        {
+            var platform = CreatePlatform(100, 100, 52);
+            var animal = new Animal(platform);
+            var result = animal.LeftTopCorner;
+            animal.Move();
+            Assert.AreEqual(result, animal.LeftTopCorner);
+        }
+
+        [Test]
+        public void MoveIfOnTheRightEdge()
+        {
+            var platform = CreatePlatform(100, 100, 52);
+            var animal = new Animal(platform);
+            var result = animal.LeftTopCorner.X - 2;
+            Move(animal, platform, 3);
+            Assert.AreEqual(result, animal.LeftTopCorner.X);
+        }
+
+        [Test]
+        public void OverEdge()
+        {
+            var platform = CreatePlatform(0, 100, 52);
+            var animal = new Animal(platform);
+            var result = animal.LeftTopCorner.X - 2;
+            Move(animal, platform, 3);
+            Assert.AreEqual(false, animal.IsAlive);
+        }
+
+        public void Move(IGameObject animal, IGameObject platform, int count)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                animal.Move();
+                platform.Move();
+            }
+        }
+
+        public Platform CreatePlatform(int x, int y, int width)
+        {
+            return new Platform(new Point(x, y), width);
         }
     }
 }
