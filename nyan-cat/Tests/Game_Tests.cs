b17c409f9c0635d623260a635e9ce8b79b57a447
odinochka3000 <38666896+odinochka3000@users.noreply.github.com>
2018-04-29 17:21:29 +0500

    Update Game_Tests.cs
    - тесты для метода FindIntersectedObject (возможно, стоит изменить названия тестов)

--- nyan-cat/Tests/Game_Tests.cs
+++ nyan-cat/Tests/Game_Tests.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +12,32 @@ namespace nyan_cat.Tests
     public class Game_Tests
     {
         [Test]
-        public void SomeTest()
+        public void Intersection()
         {
+            var food = new Food(new Point(60, 9));
+            var map = MapCreator.CreateMap(300, 300, food);
+            var game = new Game(10, 10, map);
+            Assert.AreEqual(food, game.FindIntersectedObject());
+        }
 
+        [Test]
+        public void NotIntersection()
+        {
+            var food = new Food(new Point(61, 9));
+            var map = MapCreator.CreateMap(300, 300, food);
+            var game = new Game(10, 10, map);
+            Assert.AreNotEqual(food, game.FindIntersectedObject());
+        }
+
+        [Test]
+        public void IntersectionAfterMove()
+        {
+            var food = new Food(new Point(61, 9));
+            var map = MapCreator.CreateMap(300, 300, food);
+            var game = new Game(10, 10, map);
+            Assert.AreNotEqual(food, game.FindIntersectedObject());
+            food.Move();
+            Assert.AreEqual(food, game.FindIntersectedObject());
         }
     }
 }
