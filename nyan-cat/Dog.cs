705343aa6d6d68fe859b5e3fd2d09c27c9badc36
odinochka3000 <38666896+odinochka3000@users.noreply.github.com>
2018-05-06 21:07:48 +0500

    Dog
    - Реализован метод Move() 
    - добавлен класс для поиска пути к кошке(неполная реализация)

--- nyan-cat/Dog.cs
+++ nyan-cat/Dog.cs
@@ -8,32 +8,87 @@ using System.Threading.Tasks;
 
 namespace nyan_cat
 {
+
+
     public class Dog : IGameObject
     {
-        public Vector2 Velocity { get; }
-        public Point LeftTopCorner { get; }
+        public Vector2 Velocity { get; private set; }
+        public Point LeftTopCorner { get; private set; }
         public int Height { get; }
         public int Width { get; }
-        public bool IsAlive { get; }
+        public bool IsAlive { get; private set; }
+        public bool IsMet { get; set; }
+        public Platform Location { get; private set; }
+        public int Energy { get; private set; }
+        public DogPathfinder Pathfinder { get; private set; }
+
+    public Dog(Platform platform)
+        {
+            if (platform.Width <= 50 || platform.LeftTopCorner.Y < 50)
+                throw new ArgumentException();
+            Velocity = new Vector2(-1, 0);
+            Height = 50;
+            Width = 80;
+            LeftTopCorner = GetLeftTopCorner(platform);
+            IsAlive = true;
+            Energy = 0;
+            Location = platform;
+            Pathfinder = new DogPathfinder();
+        }
 
         public void Move()
         {
-            throw new NotImplementedException();
+            throw new Exception("Wrong call!");
+        }
+
+        public void Move(Game game)
+        {
+            
+            Energy += 1;
+            if (Energy >= 30)
+            {
+                var nextPlatform = Pathfinder.FindPath(game, Location)[0];
+                Energy = 0;
+                LeftTopCorner = GetLeftTopCorner(nextPlatform);
+                return;
+            }
+
+            var dx = (int)Velocity.X;
+            var dy = (int)Velocity.Y;
+            LeftTopCorner = new Point(LeftTopCorner.X + dx,
+                LeftTopCorner.Y + dy);
+            IsAlive = IsAlive && LeftTopCorner.X > 0;
+        }
+
+        public void Kill() => IsAlive = false;
+
+        public void Accelerate(Vector2 acceleration)
+        {
+            Velocity = new Vector2(Velocity.X + acceleration.X,
+                Velocity.Y + acceleration.Y);
         }
 
         public void Use(Game game)
         {
-            throw new NotImplementedException();
+            IsMet = true;
+            if (!game.NyanCat.ProtectedFromEnemies)
+            {
+                game.Score -= 100;
+                if (game.NyanCat.CurrentGem.Kind != GemKind.MilkLongLife)
+                    game.Combo = 1 * game.AddCombo;
+            }
         }
 
-        public void Kill()
+        private Point GetLeftTopCorner(Platform platform)
         {
-            throw new NotImplementedException();
+            return new Point(platform.LeftTopCorner.X + platform.Width / 2,
+                platform.LeftTopCorner.Y - Height);
         }
 
-        public void Accelerate(Vector2 acceleration)
+
+        public override string ToString()
         {
-            throw new NotImplementedException();
+            return $"Dog ({LeftTopCorner.X}, {LeftTopCorner.Y})";
         }
     }
 }
---
+++ nyan-cat/DogPathfinder.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nyan_cat
+{
+    public class DogPathfinder
+    {
+
+        private class DijkstraData
+        {
+            public Platform Previous { get; set; }
+            public double Price { get; set; }
+        }
+
+        private static readonly Platform platformDefault =
+            new Platform(new Point(0, 0), 0);
+
+        private Platform end;
+
+        public List<Platform> FindPath(Game game, Platform start)
+        {
+            var finish = GetPlatformUnderCat(game);
+            var notVisited = new List<Platform>(GetPlatforms(game));
+            var platforms = GetPlatforms(game);
+            var track = new Dictionary<Platform, DijkstraData>
+            {
+                [start] = new DijkstraData { Price = 0, Previous = platformDefault }
+            };
+
+            while (true)
+            {
+                var toOpen = ChooseToOpen(notVisited, track);
+                if (toOpen == platformDefault)
+                    return new List<Platform>();
+                if (toOpen == finish)
+                {
+                    end = toOpen;
+                    break;
+                }
+                var directions = GetDirections(game, toOpen);
+                UpdateTrack(track, toOpen, directions);
+                notVisited.Remove(toOpen);
+            }
+            return GetResult(track);
+        }
+
+        private static void UpdateTrack(Dictionary<Platform, DijkstraData> track,
+            Platform toOpen, List<Platform> directions)
+        {
+            foreach (var point in directions)
+            {
+                var currentPrice = track[toOpen].Price + GetPrice(toOpen, point);
+                var nextNode = point;
+                if (!track.ContainsKey(nextNode) || track[nextNode].Price > currentPrice)
+                {
+                    track[nextNode] = new DijkstraData { Previous = toOpen,
+                        Price = currentPrice };
+                }
+            }
+        }
+
+        static private int GetPrice(Platform start, Platform end)
+        {
+            return 0;
+        }
+
+        private List<Platform> GetResult(Dictionary<Platform, DijkstraData> track)
+        {
+            var result = new List<Platform>();
+            while (end != platformDefault)
+            {
+                result.Add(end);
+                end = track[end].Previous;
+            }
+            result.Reverse();
+            return result.Skip(1).ToList();
+        }
+
+        private static Platform ChooseToOpen(List<Platform> notVisited,
+            Dictionary<Platform, DijkstraData> track)
+        {
+            var toOpen = platformDefault;
+            var bestPrice = double.PositiveInfinity;
+            foreach (var e in notVisited)
+            {
+                if (track.ContainsKey(e) && track[e].Price < bestPrice)
+                {
+                    bestPrice = track[e].Price;
+                    toOpen = e;
+                }
+            }
+            return toOpen;
+        }
+
+        public Platform GetPlatformUnderCat(Game game)
+        {
+            return null;
+        }
+
+        public List<Platform> GetPlatforms(Game game)
+        {
+            return game.GameObjects
+                .Where(gObj => gObj is Platform)
+                .Select(p => (Platform)p)
+                .ToList();
+        }
+
+        public List<Platform> GetDirections(Game game, Platform platform)
+        {
+
+            return null;
+        }
+    }
+}
