7226d5c168ab45e645f631a0751a89b8c997d4af
odinochka3000 <38666896+odinochka3000@users.noreply.github.com>
2018-05-08 23:24:03 +0500

    Update NyanCat_Tests.cs
    - добавлены тесты на Jump

--- nyan-cat/Tests/NyanCat_Tests.cs
+++ nyan-cat/Tests/NyanCat_Tests.cs
@@ -12,12 +12,6 @@ namespace nyan_cat.Tests
     public class NyanCat_Tests
     {
         [Test]
-        public void IncorrectCreation()
-        {
-            Assert.Throws<ArgumentException>(() => { new NyanCat(new Point(-1, 0)); });
-        }
-
-        [Test]
         public void CorrectCreation()
         {
             var cat = new NyanCat(new Point(3, 3));
@@ -38,7 +32,60 @@ namespace nyan_cat.Tests
             var cat = new NyanCat(new Point(10, 50));
             cat.Jump();
             cat.Move();
-            Assert.AreEqual(new Point(10, 40), cat.LeftTopCorner, cat.ToString());
+            Assert.AreEqual(new Point(10, 36), cat.LeftTopCorner, cat.ToString());
+            Assert.AreEqual(CatState.Jump, cat.State, cat.ToString());
+            cat.Move();
+            Assert.AreEqual(new Point(10, 36 - 13), cat.LeftTopCorner, cat.ToString()); 
+        }
+
+        [Test]
+        public void FallAfterJump()
+        {
+            var cat = new NyanCat(new Point(10, 50));
+            cat.Jump();
+            Move(cat, 15 + 1);
+            Assert.AreEqual(CatState.Fall, cat.State, cat.ToString());
+        }
+
+        [Test]
+        public void DoubleJump()
+        {
+            var cat = new NyanCat(new Point(10, 50));
+            cat.Jump();
+            Move(cat, 15 - 1);
+            cat.Jump();
+            Move(cat, 1);
+            Assert.AreEqual(CatState.Jump, cat.State, cat.ToString());
+            Move(cat, 15 - 1);
+            Assert.AreEqual(CatState.Fall, cat.State, cat.ToString());
+        }
+
+        [Test]
+        public void NotTripleJump()
+        {
+            var cat = new NyanCat(new Point(10, 50));
+            cat.Jump();
+            Move(cat, 15);
+            Assert.AreEqual(CatState.Fall, cat.State, cat.ToString());
+            cat.Jump();
+            Assert.AreEqual(CatState.Jump, cat.State, cat.ToString());
+            Move(cat, 15);
+            Assert.AreEqual(CatState.Fall, cat.State, cat.ToString());
+            cat.Jump();
+            Assert.AreEqual(CatState.Fall, cat.State, cat.ToString());
+        }
+
+        [Test]
+        public void ReloadJumpsAfterRun()
+        {
+            var cat = new NyanCat(new Point(10, 50));
+            cat.Jump();
+            cat.Jump();
+            Move(cat, 15);
+            cat.Jump();
+            Assert.AreEqual(CatState.Fall, cat.State, cat.ToString());
+            cat.State = CatState.Run;
+            cat.Jump();
             Assert.AreEqual(CatState.Jump, cat.State, cat.ToString());
         }
 
@@ -59,6 +106,12 @@ namespace nyan_cat.Tests
             cat.State = CatState.Fall;
             cat.Move();
             Assert.AreEqual(false, cat.IsAlive, cat.ToString());
+        }
+
+        public void Move (NyanCat cat, int count)
+        {
+            for (var i = 0; i < count; i++)
+                cat.Move();
         }
     }
 }
