76dbda38650df3a4def7b6c1750fd9a7f8973176
odinochka3000 <38666896+odinochka3000@users.noreply.github.com>
2018-05-13 21:05:50 +0500

    Update Milk_Tests.cs

--- nyan-cat/Tests/Milk_Tests.cs
+++ nyan-cat/Tests/Milk_Tests.cs
@@ -1,43 +1,49 @@
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using System.Drawing;
 
 namespace nyan_cat.Tests
 {
     [TestFixture]
-    public class Milk_Tests
+    public class Platform_Tests
     {
+
         [Test]
         public void CorrectCreation()
         {
-            var milk = new Milk(new Point(3, 3));
+            var platform = CreatePlatform(10, 10, 10);
         }
 
         [Test]
         public void Move()
         {
-            var milk = new Milk(new Point(10, 10));
-            milk.Move();
+            var platform = CreatePlatform(10, 10, 10);
+            platform.Move();
             var result = new Point(10 + (int)UsualGameObjectProperties.Velocity.X, 10);
-            Assert.AreEqual(result, milk.LeftTopCorner);
+            Assert.AreEqual(result, platform.LeftTopCorner);
         }
 
         [Test]
         public void OverEdge()
         {
-            var milk = new Milk(new Point(3, 3));
-            Move(milk, 4);
-            Assert.AreEqual(false, milk.IsAlive, milk.ToString());
+            var platform = CreatePlatform(2, 2, 2);
+            Move(platform, 4);
+            Assert.AreEqual(false, platform.IsAlive, platform.ToString());
         }
 
-        public void Move(Milk milk, int count)
+        public Platform CreatePlatform(int x, int y, int width)
+        {
+            return new Platform(new Point(x, y), width);
+        }
+
+        public void Move(Platform platform, int count)
         {
             for (var i = 0; i < count; i++)
-                milk.Move();
+                platform.Move();
         }
     }
 }
