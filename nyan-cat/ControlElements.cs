b9f27c464d27260a13b60cca7a23b20cf43cc455
lakrimonia <anasaturday@yandex.ru>
2018-05-13 23:19:38 +0500

    Refactoring

---
+++ nyan-cat/ControlElements.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace nyan_cat
+{
+    public static class ControlElements
+    {
+        public static Control scoreLabel = new Label
+        {
+            Text = "SCORE: ",
+            Font = new Font("Times New Roman", 19),
+            Size = new Size(105, 25),
+            Location = new Point(0, 700),
+            ForeColor = Color.Black
+        };
+        public static Control score = new Label
+        {
+            Font = new Font("Times New Roman", 19),
+            AutoSize = true,
+            Location = new Point(scoreLabel.Right, 700),
+            ForeColor = Color.Black
+        };
+        public static Control comboLabel = new Label
+        {
+            Text = "COMBO:",
+            Font = new Font("Times New Roman", 19),
+            Size = new Size(125, 30),
+            Location = new Point(score.Right + 25, 700),
+            ForeColor = Color.Black
+        };
+        public static Control combo = new Label
+        {
+            Font = new Font("Times New Roman", 19),
+            AutoSize = true,
+            Location = new Point(comboLabel.Right, 700),
+            ForeColor = Color.Black
+        };
+        public static Control gameOver = new Label
+        {
+            Font = new Font("Times New Roman", 42),
+            AutoSize = true,
+            Location = new Point(300, 350),
+            ForeColor = Color.Red
+        };
+        public static Control powerUpLabel = new Label
+        {
+            Text = "POWER UP:",
+            Font = new Font("Times New Roman", 19),
+            Size = new Size(160, 30),
+            Location = new Point(combo.Right + 25, 700),
+            ForeColor = Color.Black
+        };
+        public static PictureBox powerUp = new PictureBox
+        {
+            Image = Image.FromFile("not_exist.png"),
+            Location = new Point(powerUpLabel.Right, 690)
+        };
+        public static Control gemLabel = new Label
+        {
+            Text = "GEM:",
+            Font = new Font("Times New Roman", 19),
+            Size = new Size(80, 30),
+            Location = new Point(powerUp.Right + 25, 700),
+            ForeColor = Color.Black
+        };
+        public static PictureBox gem = new PictureBox
+        {
+            Image = Image.FromFile("not_exist.png"),
+            Location = new Point(gemLabel.Right, 690)
+        };
+        public static List<Control> Controls = new List<Control>
+        {
+            scoreLabel, score, comboLabel, combo, gameOver,
+            powerUpLabel, powerUp, gemLabel, gem
+        };
+    }
+}
--- nyan-cat/MyForm.cs
+++ nyan-cat/MyForm.cs
@@ -27,101 +27,29 @@ namespace nyan_cat
                 Invalidate();
             };
             timer.Start();
-
-            var scoreLabel = new Label
-            {
-                Text = "SCORE: ",
-                Font = new Font("Times New Roman", 19),
-                Size = new Size(105, 25),
-                Location = new Point(0, 700),
-                ForeColor = Color.Black
-            };
-            var score = new Label
-            {
-                Font = new Font("Times New Roman", 19),
-                AutoSize = true,
-                Location = new Point(scoreLabel.Right, 700),
-                ForeColor = Color.Black
-            };
-            var comboLabel = new Label
-            {
-                Text = "COMBO:",
-                Font = new Font("Times New Roman", 19),
-                Size = new Size(125, 30),
-                Location = new Point(score.Right + 25, 700),
-                ForeColor = Color.Black
-            };
-            var combo = new Label
-            {
-                Font = new Font("Times New Roman", 19),
-                AutoSize = true,
-                Location = new Point(comboLabel.Right, 700),
-                ForeColor = Color.Black
-            };
-            var gameOver = new Label
-            {
-                Font = new Font("Times New Roman", 42),
-                AutoSize = true,
-                Location = new Point(300, 350),
-                ForeColor = Color.Red
-            };
-            var powerUpLabel = new Label
-            {
-                Text = "POWER UP:",
-                Font = new Font("Times New Roman", 19),
-                Size = new Size(160, 30),
-                Location = new Point(combo.Right + 25, 700),
-                ForeColor = Color.Black
-            };
-            var powerUp = new PictureBox
-            {
-                Image = Image.FromFile("not_exist.png"),
-                Location = new Point(powerUpLabel.Right, 690)
-            };
-            var gemLabel = new Label
-            {
-                Text = "GEM:",
-                Font = new Font("Times New Roman", 19),
-                Size = new Size(80, 30),
-                Location = new Point(powerUp.Right + 25, 700),
-                ForeColor = Color.Black
-            };
-            var gem = new PictureBox
-            {
-                Image = Image.FromFile("not_exist.png"),
-                Location = new Point(gemLabel.Right, 690)
-            };
-            Controls.Add(scoreLabel);
-            Controls.Add(score);
-            Controls.Add(comboLabel);
-            Controls.Add(combo);
-            Controls.Add(gameOver);
-            Controls.Add(powerUpLabel);
-            Controls.Add(powerUp);
-            Controls.Add(gemLabel);
-            Controls.Add(gem);
-
+            foreach (var control in ControlElements.Controls)
+                Controls.Add(control);
             Paint += (sender, args) =>
             {
-                score.Text = game.Score.ToString();
-                combo.Text = game.Combo.ToString();
+                ControlElements.score.Text = game.Score.ToString();
+                ControlElements.combo.Text = game.Combo.ToString();
                 if (game.IsOver)
                 {
                     BackColor = Color.Black;
-                    gameOver.Text = "GAME OVER";
-                    scoreLabel.Location = new Point(380, gameOver.Bottom + 20);
-                    scoreLabel.ForeColor = Color.Red;
-                    score.Location = new Point(scoreLabel.Right + 20, scoreLabel.Top);
-                    score.ForeColor = Color.Red;
-                    gem.Image = null;
-                    powerUp.Image = null;
+                    ControlElements.gameOver.Text = "GAME OVER";
+                    ControlElements.scoreLabel.Location = new Point(380, ControlElements.gameOver.Bottom + 20);
+                    ControlElements.scoreLabel.ForeColor = Color.Red;
+                    ControlElements.score.Location = new Point(ControlElements.scoreLabel.Right + 20, ControlElements.scoreLabel.Top);
+                    ControlElements.score.ForeColor = Color.Red;
+                    ControlElements.gem.Image = null;
+                    ControlElements.powerUp.Image = null;
                 }
                 else
                 {
-                    powerUp.Image = Image.FromFile(game.NyanCat.CurrentPowerUp != null
+                    ControlElements.powerUp.Image = Image.FromFile(game.NyanCat.CurrentPowerUp != null
                         ? GameObjectExtensions.PowerUpImages[game.NyanCat.CurrentPowerUp.Kind]
                         : "not_exist.png");
-                    gem.Image = Image.FromFile(game.NyanCat.CurrentGem != null
+                    ControlElements.gem.Image = Image.FromFile(game.NyanCat.CurrentGem != null
                         ? GameObjectExtensions.GemImages[game.NyanCat.CurrentGem.Kind]
                         : "not_exist.png");
                     game.NyanCat.Draw(game, args.Graphics);
